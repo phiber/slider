c0f005c3e8f6cfbc828bce5b6b3f639ea2c3d776
Philipp Berger <philipp@berger.net>
2013-11-02 12:33:27 +0100

    - Speed shown as percentage - Direction can be reversed

--- SliderControl/SliderControl.ino
+++ SliderControl/SliderControl.ino
@@ -14,8 +14,10 @@ Adafruit_SSD1306 display(OLED_RESET);
 
 //AccelStepper stepper; // Defaults to AccelStepper::FULL4WIRE (4 pins) on 2, 3, 4, 5
 AccelStepper stepper(1,9,8); // 1 = type, 9 = step, 8 = direction
+const int ACCEL_STEPPER_MAX_SPEED = 1024;
 
-const int DEBUG = 1; // Debug on=1, off=0
+const int DEBUG = 0; // Debug on=1, off=0
+
 
 const int SPEED_SENSOR_PIN = 0;
 int speedSensorValue = 0;
@@ -35,7 +37,7 @@ void setup()
   display.setTextColor(WHITE);
   display.setCursor(0,0);
 
-  stepper.setMaxSpeed(1024);
+  stepper.setMaxSpeed(ACCEL_STEPPER_MAX_SPEED);
 
   //Initialize switch1 as Input
   pinMode(SWITCH_1_PIN, INPUT);
@@ -53,25 +55,39 @@ void debug(String text, int value) {
   Serial.print("\n");
 }
 
+void debug(String text, float value) {
+  if (DEBUG == 0) {
+    return;
+  }
+  Serial.print(text);
+  Serial.print(value);
+  Serial.print("\n");
+}
+
 
 int readSpeedSensorValue() {
   int speed = analogRead(SPEED_SENSOR_PIN);
-  return speed - speed % 10; // flatten sensor value
+  return map(speed,0,1023,-100,100); // percent will range from -100 to 100.
 }
 
 void showOnDisplay(String text, int value) {
   display.setCursor(0,0);
   display.clearDisplay(); 
-  display.println(text+value);
+  display.println(text+value+"%");
   display.display();
 }
 
-void changeSpeedTo(int speed) {
-  if (speed != speedSensorValue) { 
-    stepper.setSpeed(speed);
-    debug("Speed = ", speed);
-    showOnDisplay("Speed = ", speed);
-    speedSensorValue = speed;
+void changeSpeedTo(int speedPercentage) {
+  if (speedPercentage != speedSensorValue) { 
+    float maximalSpeed = ACCEL_STEPPER_MAX_SPEED;
+    float percentage = (float)speedPercentage / 100;
+    //debug("percentage = ", percentage);
+    float newSpeed = maximalSpeed * percentage; 
+    //debug("newSpeed = ", newSpeed);
+    stepper.setSpeed(newSpeed);
+
+    showOnDisplay("Speed = ", speedPercentage);
+    speedSensorValue = speedPercentage;
   }
 }
 
@@ -86,5 +102,7 @@ void loop()
   changeSpeedTo(speed);
   stepper.runSpeed();
 }
+
+
 
 
