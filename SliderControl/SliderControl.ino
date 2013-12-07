88b88cc7a93912c95cbb389f01cd332e02e7a071
Philipp Berger <philipp@berger.net>
2013-12-07 16:09:59 +0100

    reverses direction if an end switch is triggered

--- SliderControl/SliderControl.ino
+++ SliderControl/SliderControl.ino
@@ -16,13 +16,17 @@ Adafruit_SSD1306 display(OLED_RESET);
 AccelStepper stepper(1,9,8); // 1 = type, 9 = step, 8 = direction
 const int ACCEL_STEPPER_MAX_SPEED = 1024;
 
-const int DEBUG = 0; // Debug on=1, off=0
-
+const int DEBUG = 1; // Debug on=1, off=0
 
 const int SPEED_SENSOR_PIN = 0;
 int speedSensorValue = 0;
 
 const int SWITCH_1_PIN = 2;
+const int SWITCH_2_PIN = 7;
+
+const float DIRECTION_TOWARDS_SWITCH_2 = -1;
+const float DIRECTION_TOWARDS_SWITCH_1 = 1;
+float _direction = DIRECTION_TOWARDS_SWITCH_2;
 
 void setup()
 {  
@@ -41,6 +45,7 @@ void setup()
 
   //Initialize switch1 as Input
   pinMode(SWITCH_1_PIN, INPUT);
+  pinMode(SWITCH_2_PIN, INPUT);
 
   stepper.setSpeed(speedSensorValue);
 
@@ -67,7 +72,7 @@ void debug(String text, float value) {
 
 int readSpeedSensorValue() {
   int speed = analogRead(SPEED_SENSOR_PIN);
-  return map(speed,0,1023,-100,100); // percent will range from -100 to 100.
+  return map(speed,0,1023,0,100); // percent will range from -100 to 100.
 }
 
 void showOnDisplay(String text, int value) {
@@ -82,8 +87,8 @@ void changeSpeedTo(int speedPercentage) {
     float maximalSpeed = ACCEL_STEPPER_MAX_SPEED;
     float percentage = (float)speedPercentage / 100;
     //debug("percentage = ", percentage);
-    float newSpeed = maximalSpeed * percentage; 
-    //debug("newSpeed = ", newSpeed);
+    float newSpeed = maximalSpeed * percentage * _direction; 
+    debug("newSpeed = ", newSpeed);
     stepper.setSpeed(newSpeed);
 
     showOnDisplay("Speed = ", speedPercentage);
@@ -91,14 +96,38 @@ void changeSpeedTo(int speedPercentage) {
   }
 }
 
+float changeDirection() {
+  _direction = _direction * -1;
+  debug("direction changed:", _direction);
+  return _direction;
+}
+
+float getDirection() {
+  return _direction;
+}
+
+
+boolean endReached() {
+  int switch1 = digitalRead(SWITCH_1_PIN);
+  int switch2 = digitalRead(SWITCH_2_PIN);
+  return (
+    (getDirection() == DIRECTION_TOWARDS_SWITCH_2 && switch2 == HIGH)
+      ||
+    (getDirection() == DIRECTION_TOWARDS_SWITCH_1 && switch1 == HIGH)
+    );
+}
+
 void loop()
 {  
   int speed = readSpeedSensorValue();
-  int switch1 = digitalRead(SWITCH_1_PIN);
+ 
+  
   //debug("Switch1 = ", switch1);
-  if (switch1 == HIGH) {
-    speed = 0;
-  }
+  if (endReached()) {
+      changeSpeedTo(0);
+      speed = speed * changeDirection();
+  } 
+ 
   changeSpeedTo(speed);
   stepper.runSpeed();
 }
