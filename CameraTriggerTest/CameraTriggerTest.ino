92a98bc4504a3ab3193a9d56a6a3c403171babfc
Thomas Staub <mail@thomas.st>
2014-10-11 16:43:36 +0200

    Non-blocking triggering

--- CameraTriggerTest/CameraTriggerTest.ino
+++ CameraTriggerTest/CameraTriggerTest.ino
@@ -1,30 +1,33 @@
-/*
-Blink
-Turns on an LED on for one second, then off for one second, repeatedly.
 
-This example code is in the public domain.
-*/
+//int optoFocus = 3;
+int optoTrigger = 10;
+long bulbTime = 1000;
+long triggerInterval = 100;
+long triggerMillis = 0;
+bool triggerPressed = false;
+//bool focused = false;
 
-// Pin 13 has an LED connected on most Arduino boards.
-// give it a name:
-int led = 13;
-int opto = 9;
 
 // the setup routine runs once when you press reset:
 void setup()
 {
 // initialize the digital pin as an output.
-pinMode(led, OUTPUT);
-pinMode(opto, OUTPUT);
+pinMode(optoFocus, OUTPUT);
+pinMode(optoTrigger, OUTPUT);
 }
 
 // the loop routine runs over and over again forever:
 void loop()
 {
-digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-digitalWrite(opto, HIGH);
-delay(1000);               // wait for a second
-digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-digitalWrite(opto, LOW);
-delay(1000);               // wait for a second
+   if (!triggerPressed && ((unsigned long)(millis() - triggerMillis) >= triggerInterval)) {
+    digitalWrite(optoFocus, HIGH);
+    triggerMillis = millis();
+    triggerPressed = true;
+   }
+   if(triggerPressed && ((unsigned long)(millis() - triggerMillis) >= bulbTime)) {
+    digitalWrite(optoFocus, LOW);
+    triggerMillis = millis();
+    triggerPressed = false;
+   }
+   
 }
